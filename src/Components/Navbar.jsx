1d9843bf333d2a4fd90a3383a8e815d8ce608a48
MariemBenyamin <gopraeelmariem@gmail.com>
2025-07-03 20:57:51 +0300

    Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,13 @@
-
-import React from 'react'
-import { NavLink } from 'react-router' 
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
+
   return (
     <div>
       <header className="header-area header-sticky">
@@ -10,40 +15,23 @@ function Navbar() {
           <div className="row">
             <div className="col-12">
               <nav className="main-nav">
-                {/* Logo Start */}
                 <NavLink to="/" className="logo">
                   <h1>Villa</h1>
                 </NavLink>
 
-                {/* Menu Start */}
-                <ul className="nav">
+                <ul className={`nav ${menuOpen ? 'active' : ''}`}>
                   <li><NavLink to="/">Home</NavLink></li>
                   <li><NavLink to="/properties">Properties</NavLink></li>
                   <li><NavLink to="/details">Property Details</NavLink></li>
                   <li><NavLink to="/contact">Contact Us</NavLink></li>
                   <li><NavLink to="/product">Products</NavLink></li>
                   <li><NavLink to="/shop">Shop</NavLink></li>
-
-                  {/* Dropdown */}
-                  <li className="nav-item dropdown">
-                    <NavLink
-                      to="#"
-                      className="menu-btn dropdown-toggle text-white"
-                      role="button"
-                      data-bs-toggle="dropdown"  
-                      aria-expanded="false"
-                    >
-                      <i className="fa-solid fa-cart-shopping"></i> Buy & Rent 
-                    </NavLink>
-                    <div className="dropdown-menu">
-                      <NavLink to="/payment" className="dropdown-item">Payment</NavLink>
-                      <NavLink to="/success" className="dropdown-item">Success</NavLink>
-                    </div>
-                  </li>
-                  
+                  <li><NavLink to="/payment">Payment</NavLink></li>
+                  <li><NavLink to="/cart"><i className="fa-solid fa-cart-shopping"></i> Buy & Rent</NavLink></li>
+                
                 </ul>
 
-                <a className="menu-trigger">
+                <a className="menu-trigger" onClick={toggleMenu}>
                   <span>Menu</span>
                 </a>
               </nav>
